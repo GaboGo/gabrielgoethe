3516ad8ae1c181d7276389e044e2d2cca633f8fe
Leonardo Funez <leonardofunez@Leonardos-iMac.local>
2017-09-20 09:49:17 -0500

    Jquery file was added instead of cdn

--- public/javascripts/controllers.js
+++
@@ -1,6 +0,0 @@
-angular.module('AngularApp').controller('AngularCtrl', function($scope, $http){
-	$scope.jsonContenido = [];
-	/*$http.get('wp-content/themes/ecosoaps/js/informacion.json').success(function(jsonContenido){
-		$scope.contenido = jsonContenido;
-	});*/
-})
